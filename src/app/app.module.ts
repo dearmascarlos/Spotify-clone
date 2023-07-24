36cb34b80c0e0a80dbba24551307eab627624ada
dearmascarlos <correo.carlosdearmas@gmail.com>
2023-07-24 23:50:39 +0100

    structure completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './shared/components/media-player/media-player.component';
+import { HeaderUserComponent } from './shared/components/header-user/header-user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
